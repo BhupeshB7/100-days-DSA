fd355907bbce547690ef322431e48cf6471911f9
Bhupesh Kumar <96219279+BhupeshB7@users.noreply.github.com>
2025-07-06 03:43:04 +0000

    day-01

---
+++ day01.js
@@ -0,0 +1,15 @@
+function twoSum(nums, target) {
+    const order_map = new Map();
+
+   for(let i =0;i<nums.length;i++){
+
+        let first  = nums[i];
+        let sec    = target-first;
+        if(order_map.has(sec)){
+            return [order_map.get(sec),i]
+        }
+        order_map.set(first,i);
+   } 
+};
+
+console.log(twoSum([5,2,11,7,15],18));
